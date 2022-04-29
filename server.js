74a72a19493c49db9d857cae0d42968474c5838b
Amina Thiam <amina.thiam.pro@gmail.com>
2022-04-29 16:33:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/dist')));
